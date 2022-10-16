419178acf861348821dd665d432f7851d95f693a
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-10-16 22:46:28 +0600

    Three sorted values using if-else with (&&) logical operator

--- ThreeNumberSorted.cpp
+++ ThreeNumberSorted.cpp
@@ -1,7 +1,7 @@
 /**
  * @file ThreeNumberSorted.cpp
  * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
- * @brief Three sorted values using if-else
+ * @brief Three sorted values using if-else with (&&) logical operator
  * @version 0.1
  * @date 2022-10-16
  * @since SunDay; 10:00 PM
@@ -22,31 +22,31 @@ int main(int argc, char const *argv[]){
     cout<<"Enter c: ";
     cin>>c;
 
-    if((a < b) && b < c){
+    if((a <= b) && b <= c){
         cout<<"\t\tFirst Block"<<endl;
         cout<<"The big number is, c: "<<c<<endl;
         cout<<"The mid number is, b: "<<b<<endl;
         cout<<"The small number is a: "<<a<<endl;
         cout<<"Ascending Order: "<<a<<"<"<<b<<"<"<<c<<endl;
-    }else if((b < a) && (a < c)){
+    }else if((b <= a) && (a <= c)){
         cout<<"\t\tSecond Block"<<endl;
         cout<<"The big number is, c: "<<c<<endl;
         cout<<"The mid number is, a: "<<a<<endl;
         cout<<"The small number is, b: "<<b<<endl;
         cout<<"Ascending Order: "<<b<<"<"<<a<<"<"<<c<<endl;
-    }else if((a < b) && (a > c)){
+    }else if((a <= b) && (a >= c)){
         cout<<"\t\tThird Block"<<endl;
         cout<<"The big number, is: "<<b<<endl;
         cout<<"The mid number, is: "<<a<<endl;
         cout<<"The small number, is: "<<c<<endl;
         cout<<"Ascending Order: "<<c<<"<"<<a<<"<"<<b<<endl;
-    }else if((a > b) && (b < c)){
+    }else if((a >= b) && (b <= c)){
         cout<<"\t\tFourth Block"<<endl;
         cout<<"The big number, is: "<<a<<endl;
         cout<<"The mid number, is: "<<c<<endl;
         cout<<"The small number, is: "<<b<<endl;
         cout<<"Ascending Order: "<<b<<"<"<<c<<"<"<<a<<endl;
-    }else if((a > b) && (b > c)){
+    }else if((a >= b) && (b >= c)){
         cout<<"\t\tFift Block"<<endl;
         cout<<"The big number, is: "<<a<<endl;
         cout<<"The mid number, is: "<<b<<endl;
