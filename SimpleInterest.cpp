73e9ef92a9b389b30908ee8618f048edf5027e76
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-10-16 16:40:35 +0600

    Calculate simple interest, (p*n*r)/100 formula

---
+++ SimpleInterest.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file SimpleInterest.cpp
+ * @author your name (you@domain.com)
+ * @brief Calculate simple interest, (p*n*r)/100 formula
+ * @version 0.1
+ * @date 2022-10-16
+ * @since SunDay; 04:38 PM
+ * @copyright Copyright (c) 2022
+ * Dept. of CSE, Varendra University, Rajshahi, Bangladesh
+ */
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    /* code */
+    system("cls");
+    float simpleInterest, p, n, r;
+    cout<<"Enter p, n, r:"<<endl;
+    cin>>p>>n>>r;
+    simpleInterest = (p*n*r)/100;
+    cout<<"The simple interest: "<<simpleInterest<<endl;
+    return 0;
+}
