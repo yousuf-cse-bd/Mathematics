d39040892f5f2706315211b29d9a175764205b50
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-10-25 11:37:58 +0600

    Find greatest number (divisor) which divides number1 & number2 with the remainder a constant number in every time

--- FindBigDivisorGivenMod4.cpp
+++ FindBigDivisorGivenMod4.cpp
@@ -1,7 +1,7 @@
 /**
  * @file FindBigDivisorGivenMod4.cpp
  * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
- * @brief Find greatest number which divides 100 & 184 with the remainder 4 in every time
+ * @brief Find greatest number (divisor) which divides number1 & number2 with the remainder a constant number in every time
  * @version 0.1
  * @date 2022-10-25
  * @since TuesDay; 11:12 AM
@@ -14,7 +14,7 @@ using namespace std;
 class FindBigDivisor{
 public:
     /*Finding big divisor using GCD's concept*/
-    void bigDivisor(unsigned int number1, unsigned int number2, const unsigned int fixedRemainder){
+    void bigDivisorUsingGCD(unsigned int number1, unsigned int number2, const unsigned int fixedRemainder){
         number1 = number1 - fixedRemainder;
         number2 = number2 - fixedRemainder;
         unsigned int temp;
@@ -26,7 +26,7 @@ public:
         }
         cout<<"The big divisor: "<<number1<<endl;
     }
-    void bigDivisor(unsigned int number1, unsigned int number2){
+    void bigDivisorUsingIteration(unsigned int number1, unsigned int number2, const unsigned int fixedRemainder){
         unsigned int bigNumber, bigDivisor;
         if(number1 < number2){
             bigNumber = number2;
@@ -34,7 +34,7 @@ public:
             bigNumber = number1;
         }
         for(unsigned int i = 1; i <= bigNumber; i++){
-            if((number1 % i == 4) && (number2 % i == 4)){
+            if((number1 % i == fixedRemainder) && (number2 % i == fixedRemainder)){
                 bigDivisor = i;
             }
         }
@@ -46,7 +46,14 @@ int main(int argc, char const *argv[]){
     /* code */
     system("cls");
     FindBigDivisor divisor;
-    divisor.bigDivisor(100, 184);
-    divisor.bigDivisor(100, 184, 4);
+    unsigned int number1, number2, fixedRemainder;
+    cout<<"\tEnter Number1: ";
+    cin>>number1;
+    cout<<"\tEnter Number2: ";
+    cin>>number2;
+    cout<<"\tEnter Fixed Remainder: ";
+    cin>>fixedRemainder;
+    divisor.bigDivisorUsingGCD(number1, number2, fixedRemainder);
+    divisor.bigDivisorUsingIteration(number1, number2, fixedRemainder);
     return 0;
 }
